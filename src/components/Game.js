4eed302fa9f3284daf74a1a3462bab81d2d2af98
boycany <yurayurateiko@gmail.com>
2022-07-15 16:25:55 +0800

    update btn

--- src/components/Game.js
+++ src/components/Game.js
@@ -112,7 +112,7 @@ class Game extends React.Component {
         <li key={move}>
           <button 
             onClick={()=> this.jumpTo(move)} 
-            style={ this.state.stepNumber === move ? {  backgroundColor: "palegreen" }: {}}
+            className={ this.state.stepNumber === move ? "step-now": ""}
           >
             {desc}
           </button>
